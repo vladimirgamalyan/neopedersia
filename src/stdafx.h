19544dbcd72fe93a2b00832cff24532a67267978
vladimirgamalian <vladimir.gamalian@gmail.com>
2015-06-21 17:10:13 +0700

    Created msvs solution.

---
+++ src/stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <stdint.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
